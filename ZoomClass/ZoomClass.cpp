64ef14786f496e773617fd451399ae928da29e63
YleniaChitano <116096@stud.inn.no>
2020-09-01 13:16:28 +0200

    We added some code

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//using namespace std;
+int age;
+bool sw = false;
+char Initial = 'Y';
+std::string name = "Ylenia";
+double pi = 3.14;
+// All the functions can access to these variables, since they are out here, but if we had written them 
+
+int main() {
+	std::cout << "Entrer your age :";
+	std::cin >> age;
+	std::cout << "Your age is: " << age << std::endl; // alternatively, you can also write "\n";
+	std::cout << "You are young ";
+
+	std::cout << sw;
+	std::cin >> age;
+	std::cout << "s"
+
+
 }
