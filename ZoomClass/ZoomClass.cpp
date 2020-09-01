6c76fb7d4e52b91fbd5cc9005eb2a06f3bea584e
YleniaChitano <116096@stud.inn.no>
2020-09-01 13:55:00 +0200

    Newer version

--- ZoomClass/ZoomClass.cpp
+++ ZoomClass/ZoomClass.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
 
 //using namespace std;
 
 std::string Fullname = "";
-int a = 10;
+int a = 2;
 int b = 5;
 int c = 20;
 int z = 10;
@@ -11,9 +13,13 @@ int z = 10;
 // All the functions can access to these variables, since they are out here, but if we had written them 
 
 int main() {
+	//system("cls");
 
 	if (a > b) {
 		std::cout << "A is greater than B\n";
+	}
+	else {
+	std::cout << "B is greater than A\n";
 	}
 	if (a == z) {
 		std::cout << "A and Z are equal\n";
@@ -29,12 +35,12 @@ int main() {
 		std::cout << "A and B are not equal\n";
 	}
 
-	/* 
+	
 	std::cout << "Entrer your full name ";
 	std::getline(std::cin, Fullname);
 	std::cout << "Your full name is : " << Fullname;
 	
-	*/
+	
 	
 
 
